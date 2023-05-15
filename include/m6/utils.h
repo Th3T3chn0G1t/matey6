e1f116d31b87c27e48334a81a646b8ea51dae866
ttg <prs.ttg+gh@pm.me>
2023-05-15 20:00:52 +0100

    m6: add attribute helpers to utils

--- include/m6/utils.h
+++ include/m6/utils.h
@@ -6,6 +6,19 @@
 
 extern bool m6_verbose;
 
+#if defined(__clang__) || defined(__GNUC__)
+    #define M6_HAVE_GNU
+#endif
+
+#ifdef M6_HAVE_GNU
+    #if __has_attribute(packed)
+        #define M6_PACKED __attribute__((packed))
+    #endif
+    #if __has_attribute(noreturn)
+        #define M6_NORETURN __attribute__((noreturn))
+    #endif
+#endif
+
 void m6_fatal_errno(const char* msg);
 void m6_verbose_printf(const char* fmt, ...);
 
