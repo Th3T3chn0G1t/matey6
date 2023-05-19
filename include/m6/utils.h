7291e53ce85d3a5d387de994f49394c2d04a02b4
ttg <prs.ttg+gh@pm.me>
2023-05-19 09:45:24 +0100

    m6: added printf attributes

--- include/m6/utils.h
+++ include/m6/utils.h
@@ -17,10 +17,25 @@ extern bool m6_verbose;
     #if __has_attribute(noreturn)
         #define M6_NORETURN __attribute__((noreturn))
     #endif
+    #if __has_attribute(format)
+        #define M6_PRINTF(fmt, va) __attribute__((format(printf, fmt, va)))
+    #endif
+#endif
+
+#ifndef M6_PACKED
+    #define M6_PACKED
+#endif
+
+#ifndef M6_NORETURN
+    #define M6_NORETURN
+#endif
+
+#ifndef M6_PRINTF
+    #define M6_PRINTF(...)
 #endif
 
 void m6_fatal_errno(const char* msg);
-void m6_verbose_printf(const char* fmt, ...);
-M6_NORETURN void m6_fatal_printf(const char* fmt, ...);
+M6_PRINTF(1, 2) void m6_verbose_printf(const char* fmt, ...);
+M6_PRINTF(1, 2) M6_NORETURN void m6_fatal_printf(const char* fmt, ...);
 
 #endif
